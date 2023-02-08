70e52a27dbe79e46e5ed05a52101e002a7fd9021
Quratara Wijaya <quratara.w@gmail.com>
2023-02-09 00:42:43 +0700

    Modify git ignore, ports

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started on port successfully");
 });
