876bc5a28c7f5691680b118096d45991455075ad
Quratara Wijaya <quratara.w@gmail.com>
2023-02-10 14:38:51 +0700

    update

--- server.js
+++ server.js
@@ -116,6 +116,6 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(3000, () => {
   console.log("Server started on port successfully");
 });
